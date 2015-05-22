fd0f63c19eb9437101c1a65edb065bdc57d25ad8
Daley Meistrell <dstrell14@gmail.com>
2015-05-22 14:35:52 -0700

    chs 1-6 numbers, letters, variables, assignments, and methods

---
+++ calc.rb
@@ -0,0 +1,252 @@
+# puts "How many hours are there in a year?"
+
+# puts 24 * 365
+
+# puts "How many minutes are in a decade?"
+
+# puts (24 * 60 * 365 * 10)
+
+# puts "How many seconds old are you?"
+
+# puts (25 * 365 * 24 * 60 * 60)
+
+# puts 'Hello world'
+
+# puts ''
+# puts 'goodbye'
+
+# puts 'I like' + ' apple pie'
+
+# puts 'blink ' * 4
+
+# puts 12 + 12
+
+# puts '12' + '12'
+
+# puts '12 + 12'
+
+# puts 2 * 5
+
+# puts '2' * 5
+
+# puts '2 * 5'
+
+# puts 'You\'re swell'
+
+# puts "you're swell"
+
+# my_string = '...you can say that again...'
+
+# my_name = 'Daley'
+
+# puts my_string
+
+# puts my_name
+
+
+# name = 'Anya Christina Emmanuella Jenkins Harris'
+
+# puts name
+
+# puts 'My name is ' + name + '.'
+
+# puts 'Wow! ' + name
+
+# puts 'is a really long name'
+
+# composer = 'Mozart'
+# puts composer + ' was "da bomb" in his day.'
+
+# composer = 'beethoven'
+# puts 'But I prefer ' + composer + ', personally.'
+
+# my_own_var = 'just another ' + 'string'
+# puts my_own_var
+
+# my_own_var = 5 * (1+2)
+# puts my_own_var
+
+
+# r_number = 10
+
+# my_own_var = 5 * r_number
+# puts my_own_var
+
+# var1 = 8
+# var2 = var1
+# puts var1
+# puts var2
+
+# puts ''
+
+# var1 = 'eight'
+# puts var1
+# puts var2
+
+# var1 = 2
+# var2 = '5'
+
+# puts var1.to_s + var2
+
+# puts var2.to_i
+
+# puts var2.to_f
+
+# var1 = 2
+# var2 = '5'
+# puts var1.to_s + var2
+# puts var1 + var2.to_i
+
+# puts '15'.to_f
+# puts '99.99'.to_f
+# puts '99.99'.to_i
+# puts ''
+# puts '5 is my favorite number!'.to_i
+# puts 'Who asked you about 5 or whatever?'.to_i
+# puts 'Your momma did'.to_f
+# puts ''
+# puts 'stringy'.to_s
+# puts 3.to_i
+
+# puts 20
+# puts 20.to_s
+# puts '20'
+
+# puts 'Hello there, and what\'s your name?'
+# name = gets.chomp
+# puts 'Your name is ' + name + '? What a lovely name!'
+# puts 'Pleased to meet you, ' + name + '. :)'
+
+# print "What's your first name?"
+# first_name = gets.chomp
+# first_name.capitalize!
+
+# print "What's your last name?"
+# last_name = gets.chomp
+# last_name.capitalize!
+
+# print "What city are you from?"
+# city = gets.chomp
+# city.capitalize!
+
+# print "What state or province are you from?"
+# state = gets.chomp
+# state.upcase!
+
+# puts "Your name is #{first_name} #{last_name} and you're from #{city}, #{state}!"
+
+# print "What is your firstname?"
+# first_name = gets.chomp
+
+# print "What is your middle name?"
+# middle_name = gets.chomp
+
+# print "What is your last name?"
+# last_name = gets.chomp
+
+# puts "Your name is " + first_name + " " + middle_name + " " + last_name
+
+# print "What is your favorite number?"
+# num = gets.to_i
+
+# puts "Your favorite number is " + num.to_s + "?"
+# puts "Well its not bad but #{num + 1} is literally 1 better"
+# better_num = fav_num + 1
+
+# daley = 5
+
+# puts "Number " + better_num + " is a much better number!"
+
+
+# print "What\'s your favorite number?"
+# num = gets.to_i
+
+# puts "Your favorite number is #{num}?"
+# puts "Well its not bad, but #{num + 5} is way cooler"
+# puts self
+
+# puts "What's your first name?"
+# first_name = gets.chomp
+# puts "What's your middle name?"
+# middle_name = gets.chomp
+# puts "What's your last name?"
+# last_name = gets.chomp
+
+# puts "Your name is #{first_name.length + middle_name.length + last_name.length} characters long."
+
+# puts "Your name is #{name.length.to_s} characters long"
+
+# line_width = 80
+# puts('Old Mother Hubbard'.center(line_width))
+# puts('Sat in her cupboard'.center(line_width))
+# puts('Eating her curds and whey'.center(line_width))
+# puts('when along came a spider'.center(line_width))
+# puts('Who sat down beside her'.center(line_width))
+# puts('And scared her poor shoe dog away'.center(line_width))
+
+# line_width = 40
+# str = "This is MY string"
+# puts(str.ljust(line_width))
+# puts(str.center(line_width))
+# puts(str.rjust(line_width))
+# puts(str.ljust(line_width/2) + str.rjust(line_width/2))
+
+
+# print "Dammit what do you want???"
+# gets.chomp
+# puts "WHADDYYAA MEAN 'I WANT A RAISE'?!? YOU'RE FIRED!!"
+
+# width = 50
+# puts "Table of Contents".center(width)
+# puts " "
+# puts "Chapter 1: Getting Started".ljust(width/2) + "page 1".rjust(width/2)
+# puts "Chapter 2: Numbers".ljust(width/2) + "page 9".rjust(width/2)
+# puts "Chapter 3: Letters".ljust(width/2) + "page 13".rjust(width/2)
+
+# puts 5**2
+# puts 5**0.5
+# puts 7/3
+# puts 7%3
+# puts 365 % 7
+
+# puts rand
+# puts rand
+# puts rand
+# puts rand(100)
+# puts rand(100)
+# puts rand(100)
+# puts rand(1)
+# puts rand(1)
+# puts rand(1)
+# puts rand(99999999999999)
+# puts('The weatherman said there is a')
+# puts rand(101).to_s + '% chance of rain,'
+# puts('but you can never trust a weatherman')
+puts "NUMBERS".chomp
+
+srand 1976
+puts(rand(100))
+puts(rand(100))
+puts(rand(100))
+puts(rand(100))
+puts " "
+srand 1976
+puts(rand(100))
+puts(rand(100))
+puts(rand(100))
+puts(rand(100))
+puts " "
+srand 2004
+puts(rand(100))
+puts(rand(100))
+puts(rand(100))
+puts(rand(100))
+puts " "
+srand 2004
+puts(rand(100))
+puts(rand(100))
+puts(rand(100))
+puts(rand(100))
+
+
+
